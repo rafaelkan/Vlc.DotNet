da79b47e7c12093285b81125b1fd323cbdfc22ad
rafaelkan <rafaelillopalma@hotmail.com>
2016-01-26 09:27:51 +0100

    MSBuild is actually installed with the .NET Framework

--- src/Build.bat
+++ src/Build.bat
@@ -3,8 +3,11 @@ cls
 @rem Add path to MSBuild Binaries
 @if exist "%ProgramFiles%\MSBuild\12.0\bin" set PATH=%ProgramFiles%\MSBuild\12.0\bin;%PATH%
 @if exist "%ProgramFiles(x86)%\MSBuild\12.0\bin" set PATH=%ProgramFiles(x86)%\MSBuild\12.0\bin;%PATH%
+@if exist "%WINDIR%\Microsoft.Net\Framework\v4.0.30319" set PATH=%WINDIR%\Microsoft.Net\Framework\v4.0.30319;%PATH%
 
 msbuild Vlc.DotNet.sln /t:Rebuild /p:Configuration=Debug;Platform=x86
 msbuild Vlc.DotNet.sln /t:Rebuild /p:Configuration=Release;Platform=x86
+
+@if exist "%WINDIR%\Microsoft.Net\Framework64\v4.0.30319" set PATH=%WINDIR%\Microsoft.Net\Framework64\v4.0.30319;%PATH%
 msbuild Vlc.DotNet.sln /t:Rebuild /p:Configuration=Debug;Platform=x64
 msbuild Vlc.DotNet.sln /t:Rebuild /p:Configuration=Release;Platform=x64
